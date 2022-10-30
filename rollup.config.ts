021d1c18f21dbe951efbea19b9f333689604f489
Felix Wieland <felix.wieland@mail.de>
2022-10-30 23:53:25 +0100

    updated readme and rollup conf

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,8 @@
 import withSolid from 'rollup-preset-solid';
+import css from "rollup-plugin-import-css";
 
 export default withSolid({
   input: 'src/index.tsx',
   targets: ['esm', 'cjs'],
+  plugins: [css()]
 });
